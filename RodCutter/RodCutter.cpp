2c3ab2f981514a615a6c2b63c68390144d3c474d
Hassan @Ubuntu <hassansalehe@gmail.com>
2020-11-15 21:31:42 +0100

    Refactor finding num of ways into helper function

--- RodCutter/RodCutter.cpp
+++ RodCutter/RodCutter.cpp
@@ -2,22 +2,8 @@
 #include <algorithm>
 #include <iostream>
 
-// Computation O(n^2), space O(n)
-int RodCutter(const std::vector<int> & prices) {
-    
-    std::vector<int>values(prices.size(), 0);
-
-    for (int i = 1; i < prices.size(); i++) {
-        int max_price = 0;
-        
-        for (int j = 1; j <= i; j++) {
-            max_price = std::max(max_price, prices[j] + values[i - j]);
-        }
-        values[i] = max_price;
-    }
-
-    // Compute number of ways with maximum value
-    int total_value = values[prices.size() - 1];
+void computeNumWaysWithMaxValue(const std::vector<int> & prices,
+                                const std::vector<int> & values) {
     std::vector<int>num_maximums(prices.size(), 0);
     num_maximums[0] = 1;
     int num_ways = 0;
@@ -32,6 +18,24 @@ int RodCutter(const std::vector<int> & prices) {
         }
     }
     std::cout << "== Number of ways with max value: " << num_maximums[prices.size()-1] << std::endl;
+}
 
+// Computation O(n^2), space O(n)
+int RodCutter(const std::vector<int> & prices) {
+
+    std::vector<int>values(prices.size(), 0);
+
+    for (int i = 1; i < prices.size(); i++) {
+        int max_price = 0;
+
+        for (int j = 1; j <= i; j++) {
+            max_price = std::max(max_price, prices[j] + values[i - j]);
+        }
+        values[i] = max_price;
+    }
+
+    // Compute number of ways with maximum value
+    computeNumWaysWithMaxValue(prices, values);
+    int total_value = values[prices.size() - 1];
     return total_value;
 }
